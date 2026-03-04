3017ced69d6875609701ae0a4fa4733f72fd71ae
Drustan <drustanyjt@gmail.com>
2026-03-04 23:43:53 +0800

    init

---
+++ simulation.py
@@ -0,0 +1,20 @@
+# Parameters needed
+
+class Simulator:
+    def __init__(self, hospitals, ambulance_posts, num_ambulances):
+        self.hospitals = hospitals
+        self.ambulance_posts = ambulance_posts
+        self.num_ambulances = num_ambulances
+    
+    def load_emergency_calls(self, emergency_call_data):
+        # This function would load or generate emergency call data
+        pass
+
+    def load_ambulance_allocations(self, allocation_data):
+        # This function would load 
+        pass
+
+    def run_simulation(self):
+        # This function would run the simulation using the integer programming model
+        pass
+    
